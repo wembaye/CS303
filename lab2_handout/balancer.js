45cbe77f2a9f72d7e14c4a2e3c5a1926cca1638a
wembaye <35547821+wembaye@users.noreply.github.com>
2019-11-05 07:14:38 -0600

    Update balancer.js

--- lab2_handout/balancer.js
+++ lab2_handout/balancer.js
@@ -10,14 +10,14 @@ class SymbolBalancer {
     isBalanced(del) {
         let par;
         for (let i = 0; i < del.length; i++) {
-            if (del[i] == "{" || del[i] == "("  || del[i] == "[") {
-                this.stack.storage.push(del[i]);
+            if (del[i] == "{" || del[i] == "("  || del[i] == "[" || del[i] == "<") {
+                this.stack.push(del[i]);
             }
-            else if (del[i] == "}" || del[i] == ")" || del[i] == "]") {
-                par = this.stack.storage.pop();
+            else if (del[i] == "}" || del[i] == ")" || del[i] == "]" || del[i] == ">") {
+                par = this.stack.pop();
 
                 if (!((par == "{" && del[i] == "}") || (par == "(" && del[i] == ")")
-                 || (par == "[" && del[i] == "]"))) {
+                 || (par == "[" && del[i] == "]") || (par == "<" && del[i] == ">"))) {
                     return false;
                 }
             }
@@ -30,26 +30,7 @@ class SymbolBalancer {
             return false;
         }
 
-        // TODO impelement code here
+        
 
     }
 }
-// let text = `
-// function sum(array) {
-//     let sum = 0;
-//     for (const num of array) {
-//         sum += num;
-//     }
-//     return sum;
-// }
-// function min(array) {
-//     if (array.length > 1) {
-//         let other = min(array.splice(1));
-//         if (other < array[0]) {
-//             return other;
-//         }   
-//     } 
-//     return array[0];
-// }
-// `;
-// console.log(stack.isBalanced(text));
